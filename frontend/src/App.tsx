0b3af1e28444bd14aae70f64ac812b37dabc3b6b
Dominic <andro5@web.de>
2022-01-30 18:08:38 +0100

    FRONTEND - Added BrowserRouter to the App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
 import RegistrationPage from "./Pages/RegistrationPage";
-
+import LoginPage from "./Pages/LoginPage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
 
 export default function App() {
   return (
     <div className="App">
-      <RegistrationPage/>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/login"} element={<LoginPage />} />
+                <Route path={"/registration"} element={<RegistrationPage />} />
+                <Route path={"/dashboard"} element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
