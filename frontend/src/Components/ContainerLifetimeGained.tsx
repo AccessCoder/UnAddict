f84d8e435b398acdadcebe4ebb2c57ebdcebe63a
Dominic <andro5@web.de>
2022-01-30 19:58:29 +0100

    FRONTEND - Added funcionality to ContainerLifetimeGained.tsx

--- frontend/src/Components/ContainerLifetimeGained.tsx
+++ frontend/src/Components/ContainerLifetimeGained.tsx
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export default function ContainerLifetimeGained(){
+
+    const [gainedLifetime, setGainedLifetime] = useState(0)
+
+    useEffect(() => {
+        axios.get('/api/lifetime')
+            .then(response => response.data)
+            .then(gainedLifetime => setGainedLifetime(gainedLifetime))
+            .catch(error => console.error(error))
+    }, [])
+
     return(
         <div className={"outerContainer"}>
             <div className={"textContainer"}>
-                <h3>xx Days</h3>
+                <h3>{gainedLifetime} Days</h3>
                 <h6>Lifetime gained</h6>
             </div>
         </div>
