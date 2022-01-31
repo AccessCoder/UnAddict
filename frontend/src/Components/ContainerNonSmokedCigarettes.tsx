d38828a2736a5fa746b74fb7e3594c57ee6d0797
Dominic <andro5@web.de>
2022-01-31 15:25:05 +0100

    FRONTEND - Added API Calls to the (at the moment inexistent) Backend using axios

--- frontend/src/Components/ContainerNonSmokedCigarettes.tsx
+++ frontend/src/Components/ContainerNonSmokedCigarettes.tsx
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export default function ContainerNonSmokedCigarettes(){
+
+    const [nonSmoked, setNonSmoked] = useState(0)
+
+    useEffect(() => {
+        axios.get('/api/nonsmoked')
+            .then(response => response.data)
+            .then(nonSmoked => setNonSmoked(nonSmoked))
+            .catch(error => console.error(error))
+    }, [])
+
     return(
         <div className={"outerContainer"}>
             <div className={"textContainer"}>
-                <h3>xx Cigarettes</h3>
+                <h3>{nonSmoked} Cigarettes</h3>
                 <h6>Not smoked!</h6>
             </div>
         </div>
--- frontend/src/Components/ContainerSavedMoney.tsx
+++ frontend/src/Components/ContainerSavedMoney.tsx
@@ -1,8 +1,21 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 export default function ContainerSavedMoney() {
+
+    const [savedMoney, setSavedMoney] = useState(0)
+
+    useEffect(() => {
+        axios.get('/api/savedmoney')
+            .then(response => response.data)
+            .then(savedMoney => setSavedMoney(savedMoney))
+            .catch(error => console.error(error))
+    }, [])
+
     return (
         <div className={"outerContainer"}>
             <div className={"textContainer"}>
-                <h3>xx EURO</h3>
+                <h3>{savedMoney} EURO</h3>
                 <h6>Saved by not smoking</h6>
             </div>
         </div>
