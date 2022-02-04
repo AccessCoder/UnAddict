54f12847739f496a6cf2bce7afb399f0f57f40d6
Dominic <andro5@web.de>
2022-02-04 17:14:25 +0100

    Backend - Changed token to principal to fix the flow of the System. Fixed method calls with dependency injection.

--- backend/src/main/java/de/unaddict/backend/controller/DashboardController.java
+++ backend/src/main/java/de/unaddict/backend/controller/DashboardController.java
@@ -1,12 +1,15 @@
 package de.unaddict.backend.controller;
 
 
+import de.unaddict.backend.modules.UserData;
 import de.unaddict.backend.services.DashboardService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.text.ParseException;
 
 @RestController
@@ -21,18 +24,20 @@ public class DashboardController {
 
     @GetMapping("/lifetime")
     public int getGainedLifetime() {
+
         return 5;
     }
 
     @GetMapping("/nonsmoked")
-    public double getNonSmoked(@RequestBody String token) throws ParseException {
-        return service.getNonSmokedCigarettes(token);
+    public double getNonSmoked(UsernamePasswordAuthenticationToken authToken) throws ParseException {
+        UserData user = (UserData) authToken.getPrincipal();
+        return service.getNonSmokedCigarettes(user);
     }
 
-    @GetMapping("/savedmoney")
-    public double getSavedMoney(@RequestBody String token) throws ParseException {
-        return getNonSmoked(token)*5;
-    }
+//    @GetMapping("/savedmoney")
+//    public double getSavedMoney(Principal principal) throws ParseException {
+////        return getNonSmoked(principal.getName())*5;
+//    }
 
     @GetMapping("timesmokefree")
     public String getTimeNonSmoked() throws ParseException {
--- backend/src/main/java/de/unaddict/backend/services/DashboardService.java
+++ backend/src/main/java/de/unaddict/backend/services/DashboardService.java
@@ -5,6 +5,7 @@ import de.unaddict.backend.modules.SmokeDataAPI;
 import de.unaddict.backend.modules.UserData;
 import de.unaddict.backend.repositories.IUserDataRepository;
 import de.unaddict.backend.security.JWTUtils;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,28 +14,20 @@ import java.text.ParseException;
 @Service
 public class DashboardService {
 
-    IUserDataRepository repository;
-    MongoUserDetailsService mongoUserDetailsService = new MongoUserDetailsService(repository);
+    private final MongoUserDetailsService mongoUserDetailsService;
+    private final SmokeDataAPI api;
 
-    JWTUtils jwtUtils;
-
-    UserData user = new UserData();
-
-    @Autowired
-     final SmokeDataAPI api;
-
-    public DashboardService(SmokeDataAPI api) {
+    public DashboardService(MongoUserDetailsService mongoUserDetailsService, SmokeDataAPI api) {
+        this.mongoUserDetailsService = mongoUserDetailsService;
         this.api = api;
     }
-
 
 
     public String getTimeNotSmoked(String testTime) throws ParseException {
         return api.getTimeNotSmoked(testTime);
     }
 
-    public double getNonSmokedCigarettes(String token) throws ParseException {
-       user = mongoUserDetailsService.loadUserByMail(jwtUtils.extractUserEMail(token));
+    public double getNonSmokedCigarettes(UserData user) throws ParseException {
         return api.getNonSmokedCigarettes(user.getCigarettesSmokedEachDayLastYear(), user.getUserRegistrationTime());
     }
 }
