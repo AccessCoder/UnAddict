84625a85435eefd6288db448ec2ea63d056d7661
Dominic <andro5@web.de>
2022-02-17 17:56:58 +0100

    DESIGN - CSS update on the Dashboard + LoginPage CSS

--- frontend/src/Components/LoginElement.tsx
+++ frontend/src/Components/LoginElement.tsx
@@ -2,8 +2,9 @@ import {Button, Link, TextField} from "@mui/material";
 import {useState} from "react";
 import ILoginData from "../Models/ILoginData";
 import {postLogin, TOKEN_STORAGE_KEY} from "../Service/AxiosServiceToBackend";
+import "./LoginElement.css"
 
-export default function LoginElement(){
+export default function LoginElement() {
     const [userEMail, setUserEMail] = useState("");
     const [userPassword, setUserPassword] = useState("");
 
@@ -11,20 +12,27 @@ export default function LoginElement(){
         const loginData: ILoginData = {email: userEMail, password: userPassword}
         postLogin(loginData)
             .then(response => localStorage.setItem(TOKEN_STORAGE_KEY, response.data))
-            .then(() => window.location.href="/dashboard")
+            .then(() => window.location.href = "/dashboard")
             .catch(error => console.error(error))
     }
 
     return (
-        <div className={"loginElements"}>
-            <TextField variant="outlined" label="E-Mail" type="eMail" value={userEMail}
-                       onChange={(e) => setUserEMail(e.target.value)}/>
-            <TextField variant="filled" label="Password" type="password" value={userPassword}
-                       onChange={(e) => setUserPassword(e.target.value)}/>
-            <Button onClick={() => login()} variant="outlined">Login</Button>
-            <Link href="registration" underline="hover" >
-                {'New here? - Register now, for free!'}
-            </Link>
+        <div className={"loginComponentsMaster"}>
+            <div className={"loginElements"}>
+                <br/>
+                <TextField variant="outlined" label="E-Mail" type="eMail" value={userEMail}
+                           onChange={(e) => setUserEMail(e.target.value)}/>
+                <TextField variant="filled" label="Password" type="password" value={userPassword}
+                           onChange={(e) => setUserPassword(e.target.value)}/>
+            </div>
+            <br/>
+            <div className={"loginButton"}>
+                <Button onClick={() => login()} variant="outlined">Login</Button>
+                <br/>
+                <Link href="registration" underline="hover">
+                    {'New here? - Register now, for free!'}
+                </Link>
+            </div>
         </div>
     )
 }
